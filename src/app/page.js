60596fe3eaf7da3dbfb20b219608c87d40e38c6c
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-03-06 17:00:39 +0530

    header

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <div>
-    <h1>hello this is praven</h1>
-    <Button>hello</Button>
+    home
    </div>
   );
 }
