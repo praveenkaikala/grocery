c5709151dcbbe730c6d7df3760641f550b66e4ac
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-03-09 09:56:09 +0530

    getslider api

--- src/app/_utils/GlobalApi.jsx
+++ src/app/_utils/GlobalApi.jsx
@@ -2,10 +2,15 @@ const { default: axios } = require("axios");
 
 const AxiosPrivate=axios.create(
     {
-        baseURL:"http://localhost:1337/api",
+        baseURL:"http://192.168.210.18:1337/api",
     }
 )
-export const baseUrl="http://localhost:1337"
+export const baseUrl="http://192.168.210.18:1337"
 export const getCategory=()=>AxiosPrivate.get("/categories/?populate=*");
+export const getSliders=()=>AxiosPrivate.get('/sliders/?populate=*').then((data)=>{
+    return data.data.data;
+}).catch((error)=>{
+    console.log(error)
+})
 
 
