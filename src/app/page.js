8d148375ca2564bac42389914efe8b46b14c3e18
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-03-17 11:14:41 +0530

    category list

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 "use server"
+import CategoryList from "./_components/CategoryList";
 import Slider from "./_components/Slider";
-import { getSliders } from "./_utils/GlobalApi";
+import { getCategory, getSliders } from "./_utils/GlobalApi";
 
 export default async function Home () {
   const sliders=await getSliders();
+  const categories=await getCategory().then((data)=>{
+    return data.data.data;
+  }).catch((error)=>{
+    console.log(error);
+  })
+  // console.log("era",categories);
   return (
    <div className="md:p-10 md:px-16 p-5 px-10 overflow-scroll h-[100vh]">
     <Slider sliders={sliders}/>
+    <CategoryList categories={categories}/>
    </div>
   );
 }
