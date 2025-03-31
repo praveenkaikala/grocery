774718128110239aab6e950a10b44eab7b63a8cc
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-03-31 14:29:43 +0530

    product api

--- src/app/_utils/GlobalApi.jsx
+++ src/app/_utils/GlobalApi.jsx
@@ -2,15 +2,22 @@ const { default: axios } = require("axios");
 
 const AxiosPrivate=axios.create(
     {
-        baseURL:"http://192.168.210.18:1337/api",
+        baseURL:"http://192.168.209.18:1337/api",
     }
 )
-export const baseUrl="http://192.168.210.18:1337"
+export const baseUrl="http://192.168.209.18:1337"
 export const getCategory=()=>AxiosPrivate.get("/categories/?populate=*");
 export const getSliders=()=>AxiosPrivate.get('/sliders/?populate=*').then((data)=>{
     return data.data.data;
 }).catch((error)=>{
     console.log(error)
+})
+
+
+export const getAllProducts=()=>AxiosPrivate.get("/products/?populate=*").then((data)=>{
+    return data.data.data;
+}).catch((err)=>{
+    console.log(err);
 })
 
 
