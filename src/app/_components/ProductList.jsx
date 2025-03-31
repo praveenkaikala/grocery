706763a7c243c3733e24913bc248f552644721b6
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-03-31 14:50:43 +0530

    product list

--- src/app/_components/ProductList.jsx
+++ src/app/_components/ProductList.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import Header from './styles/Header';
+import Image from 'next/image';
+import { baseUrl } from '../_utils/GlobalApi';
+import { Button } from '@/components/ui/button';
 
 const ProductList = ({products}) => {
     console.log("products",products);
   return (
     <div className='mt-5'>
         <Header heading={"Popular Products"}/>
+        <div className='grid md:grid-cols-3 grid-cols-2 lg:grid-cols-4 py-4 gap-3'>
+                {
+                    products && (
+                        products.map((item,key)=>{
+                            return(
+                                <div className='flex flex-col items-center width-auto group cursor-pointer justify-between border p-3 rounded-md transition-all ease-in-out my-3' key={key}>
+                                    <Image
+                                    src={`${baseUrl}${item?.images[0]?.url}`}
+                                    width={500}
+                                    height={200}
+                                    unoptimized={true}
+                                    alt={item?.name || "Category"}
+                                    loading="lazy"
+                                    className='group-hover:scale-105 transition-all ease-in-out bg-white w-40 h-50 rounded-lg'
+                                  />
+                                  <div className='flex flex-col items-center gap-1'>
+
+
+                                  <h2 className='font-semibold text-green-700'>{item?.name}</h2>
+                                  <div className='flex gap-2'>
+                                  <h2 className='font-extralight line-through'>${item?.mrp}</h2>
+                                  <h2 className=''>${item?.sellingPrice}</h2>
+                                    </div>
+                                    <Button variant="outline">Add To Cart</Button>
+                                  </div>
+                                </div>
+                            )
+                        })
+                    )
+                }
+                </div>
+        
     </div>
   )
 }
